69755963bcef047118cb7cb25039fe9155e2cb8d
mekas <eka.suryana@gmail.com>
2020-04-20 06:39:41 +0700

    add features num to stats branch

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from mnist import MNIST
 import numpy as np 
+from lib import *
 
 mndata = MNIST('./data')
 images, labels = mndata.load_training()
@@ -10,19 +11,4 @@ t_images = np.array(t_images)
 labels = np.array(labels)
 t_labels = np.array(t_labels)
 
-print("training instances:", images.shape[0])
-print("testing instances:", t_images.shape[0])
-
-num_class = len(np.unique(labels))
-
-print("There are", num_class,"classes")
-
-print("statistics for training")
-
-for i in range(num_class):
-    print("class", i, "instances: ", np.sum(labels==i))
-
-print("statistics for testing") 
-
-for i in range(num_class):
-    print("class", i, "instances: ", np.sum(t_labels==i))
+print_statistics(images, t_images, labels, t_labels)
